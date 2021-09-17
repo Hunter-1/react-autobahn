18eae55336fd32f40b0626ac87086ac814c86526
Hunter <hunterinswitzerland@gmail.com>
2021-09-17 12:51:08 +0200

    Added Title

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AutobahnList from "./components/autobahnList";
 function App() {
   return (
     <div className="App">
+        <h1>Liste von Autobahnen und deren Baustellen</h1>
       <AutobahnList />
     </div>
   );
